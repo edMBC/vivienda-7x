dbde46105c97087fb7da45cc19ac6a8998f6cd2f
deiver20 <deivercamiloprieto@gmail.com>
2026-07-25 19:22:49 -0500

    cambios deiver

--- backend/config.py
+++ backend/config.py
@@ -6,6 +6,6 @@ try:
     load_dotenv(Path(__file__).parent / ".env")
 except ImportError:
     pass
-
+#url
 SUPABASE_URL = os.environ.get("SUPABASE_URL", "")
 SUPABASE_KEY = os.environ.get("SUPABASE_KEY", "")
